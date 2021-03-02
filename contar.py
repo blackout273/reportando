909612ec56ab8ebc0877fa021e4dc07d803f66ae
blackout273 <69636059+blackout273@users.noreply.github.com>
2021-03-02 11:46:25 -0300

    att contar.py

--- contar.py
+++ contar.py
@@ -2,7 +2,7 @@ def app(environ, start_response):
     """Simplest possible application object"""
     data = b'Hello, World!\n'
     status = '200 OK'
-    headers = [('Content-type', 'text/plain')]
+    headers = [('Content-type', 'text/html')]
     start_response(status, headers)
     return [data]
 
