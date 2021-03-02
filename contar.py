07760e34b0db0248422cad610c6b1628e4133091
blackout273 <69636059+blackout273@users.noreply.github.com>
2021-03-02 16:48:39 -0300

    Atualização de codigo-fonte

--- contar.py
+++ contar.py
@@ -1,7 +1,6 @@
 def app(environ, start_response):
     """Simplest possible application object"""
     dados=open("index.html", mode="r")
-    str(dados).encode()
     data=dados.read()
     status = '200 OK'
     headers = [('Content-type', 'text/html')]
