7e0b3332b33a7a91ff77066a5158b9a92c6bc1e6
blackout273 <69636059+blackout273@users.noreply.github.com>
2021-03-02 17:13:19 -0300

    Atualização de codigo-fonte

--- contar.py
+++ contar.py
@@ -1,8 +1,40 @@
 def app(environ, start_response):
     """Simplest possible application object"""
-    dados=open("index.html", mode="r")
-    data=dados.read()
-    bytes(data, encoding='utf8')
+    data = b'<!DOCTYPE html>'
+    b'<html lang="pt-br">'
+    b'<head>'
+    b'<title>Doc HTML na mão é mole kk</title>'
+    b'<style>'
+    b'*{'
+    b'margin:0;'
+    b'padding:0;'
+    b'}'
+    b'.container{'
+    b'position:absolute;'
+    b'margin-left:50%;'
+    b'transform:translateX(-50%);'
+    b'margin-top:15%;'
+    b'font-size:2vw;'
+    b'text-align:center;'
+    b'}'
+    b'</style>'
+    b'</head>'
+    b'<body>'
+    b'<div class="container">'
+    b'<h1>Pipeline Completa</h1>'
+    b'<h6>por favor insira o seu nome</h6>'
+    b'<input type="text" id="nome" placeholder="Por favor, insira o seu nome">'
+    b'<button type="submit" onclick=(botao())>enviar</button> '
+    b'</div>'
+    b'</body>'
+    b'<script type="text/javascript">'
+    b'var nome = document.getElementById("nome");'
+    b'function botao(){'
+    b'alert(+nome.value);'
+    b'}'
+    b'</script>'
+    b'</html>'
+
     status = '200 OK'
     headers = [('Content-type', 'text/html')]
     start_response(status, headers)
