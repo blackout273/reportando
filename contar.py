998526bb83862a7cf0a79caf0d75fffd242d7b66
blackout273 <69636059+blackout273@users.noreply.github.com>
2021-03-02 11:53:40 -0300

    Atualização de codigo-fonte

--- contar.py
+++ contar.py
@@ -1,7 +1,7 @@
 def app(environ, start_response):
     """Simplest possible application object"""
-    data = b'Hello, World!\n'
     status = '200 OK'
+    data = b'<h1>Pagina Teste , Status resposta= {status}</h1>\n'.__format__(status)
     headers = [('Content-type', 'text/html')]
     start_response(status, headers)
     return [data]
