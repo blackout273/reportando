f442b6d84364acaa2c4547bb7526179f87ceaaa3
blackout273 <69636059+blackout273@users.noreply.github.com>
2021-03-02 13:47:02 -0300

    Atualização de codigo-fonte

--- contar.py
+++ contar.py
@@ -1,7 +1,7 @@
 def app(environ, start_response):
     """Simplest possible application object"""
-    status = input("Insira uma frase")
-    data = status.encode('utf-8')
+    data = b"<h1>Fim da pipeline</h1><button id='btn' type='submit'>enviar</button><script>btn = document.getElementsById('btn') if(btn){alert('apertou botao')}</script>"
+    status = '200 OK'
     headers = [('Content-type', 'text/html')]
     start_response(status, headers)
     return [data]
