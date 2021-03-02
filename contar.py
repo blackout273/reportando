b3367c3d577559e01a498e335e050fd8aaef1aff
blackout273 <69636059+blackout273@users.noreply.github.com>
2021-03-02 13:38:07 -0300

    Atualização de codigo-fonte

--- contar.py
+++ contar.py
@@ -1,6 +1,6 @@
 def app(environ, start_response):
     """Simplest possible application object"""
-    status = '200 OK'
+    status = input("Insira uma frase")
     data = status.encode('utf-8')
     headers = [('Content-type', 'text/html')]
     start_response(status, headers)
