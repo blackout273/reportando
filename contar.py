be2f15fe8271703330ad751b79b27bd8624afa86
blackout273 <69636059+blackout273@users.noreply.github.com>
2021-03-02 17:51:57 -0300

    Atualização de codigo-fonte

--- contar.py
+++ contar.py
@@ -1,6 +1,26 @@
 def app(environ, start_response):
     """Simplest possible application object"""
-    data = b"<!DOCTYPE html><html=lang'pt-br'><head><title>Pipeline</title><style>*{margin:0;padding:0;}.container{position:absolute;margin-left:50%;transform:translateX(-50%);margin-top:15%;font-size:2vw;text-align:center;}</style></head><body><div class='container'><h1>Pipeline Completa</h1><h6>por favor insira o seu nome</h6><input type='text' id='nome' placeholder='Por favor, insira o seu nome'><button type='submit' onclick=(botao())>enviar</button> </div></body><script type='text/javascript'>var nome = document.getElementById('nome');function botao(){alert('ola'+nome.value);}</script></html>"
+    data = b"<!DOCTYPE html>" \
+           b"<html=lang'pt-br'>" \
+           b"<head><title>Pipeline</title>" \
+           b"<style>*{" \
+           b"margin:0;padding:0;}" \
+           b".container{position:absolute;margin-left:50%;transform:translateX(-50%);margin-top:15%;font-size:2vw;text-align:center;}" \
+           b"</style></head>" \
+           b"<body>" \
+           b"<div class='container'><h1>Pipeline Completa</h1>" \
+           b"<h6>por favor insira o seu nome</h6>" \
+           b"<input type='text' id='nome' placeholder='Por favor, insira o seu nome'>" \
+           b"<button type='submit' onclick=(botao())>enviar</button>" \
+           b" </div>" \
+           b"</body>" \
+           b"<script type='text/javascript'>" \
+           b"var nome = document.getElementById('nome');" \
+           b"function botao(){" \
+           b"alert('ola: '+nome.value);" \
+           b"}</script>" \
+           b"</html>"
+
     status = '200 OK'
     headers = [('Content-type', 'text/html')]
     start_response(status, headers)
