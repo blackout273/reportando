dc23949628aa819d75754dd81f2d4cfc59fe6dfd
blackout273 <69636059+blackout273@users.noreply.github.com>
2021-03-03 11:32:25 -0300

    Att contar.py

--- contar.py
+++ contar.py
@@ -1,12 +1,74 @@
 def app(environ, start_response):
     """Simplest possible application object"""
 
-    data = b"<html lang='pt-br'>    <head>    <meta charset='utf-8'>    <title>Pipeline</title>    <style>    *{    margin:0;    padding:0;    }    .container{    position:absolute;    margin-left:50%;    transform:translateX(-50%);    margin-top:15%;    font-size:2vw;    text-align:center;}    .btn{        border: none;        background-color: white;        color: black;        width: 2vw;        height: 2vh;        border-radius: 10px;        transition: 0.5s ease;    }    .btn:hover{        background-color: black;        color: white;        border: 2px solid white;        transition: 0.3s ease;    }    body{        transition: 0.5s ease;        font-family: 'Courier New', Courier, monospace;    }    body:hover{        background-color: black;        color: white;        transition: 0.3s ease;    }    </style></head>    <body>    <div class='container'><h3>Calcule o seu indice de Massa Corporal(IMC)</h3>    <h6 id='cobaia'></h6><br>    <input type='number' id='peso' placeholder='Peso' style='text-align: center;width: 10vw;height: 2vh;'><br><br>    <input type='number' id='altura' placeholder='Altura , em centimetros' style='text-align: center;width: 10vw;height: 2vh;'><br><br>    <button type='submit' class='btn' onclick=(botao())>GO</button>     </div>    </body>    <footer> <script type='text/javascript'>" \
+    data = b"<html lang='pt-br'>    " \
+           b"<head>    " \
+           b"<meta charset='utf-8'>    " \
+           b"<title>Pipeline</title>    " \
+           b"<style>    " \
+           b"*{    " \
+           b"margin:0;    " \
+           b"padding:0;    " \
+           b"}    " \
+           b".container{    " \
+           b"position:absolute;    " \
+           b"margin-left:50%;    " \
+           b"transform:translateX(-50%);    " \
+           b"margin-top:15%;    " \
+           b"font-size:2vw;    " \
+           b"text-align:center;" \
+           b"}    " \
+           b".btn{        " \
+           b"border: none;        " \
+           b"background-color: white;        " \
+           b"color: black;        " \
+           b"width: 2vw;        " \
+           b"height: 2vh;        " \
+           b"border-radius: 10px;        " \
+           b"transition: 0.5s ease;    " \
+           b"}    " \
+           b".btn:hover{        " \
+           b"background-color: black;        " \
+           b"color: white;        " \
+           b"border: 2px solid white;        " \
+           b"transition: 0.3s ease;    " \
+           b"}    " \
+           b"body{        " \
+           b"transition: 0.5s ease;        " \
+           b"font-family: 'Courier New', Courier, monospace;    " \
+           b"}    " \
+           b"body:hover{        " \
+           b"background-color: black;        " \
+           b"color: white;        " \
+           b"transition: 0.3s ease;    " \
+           b"}    " \
+           b"</style>" \
+           b"</head>    " \
+           b"<body>    " \
+           b"<div class='container'><h3>Calcule o seu indice de Massa Corporal(IMC)</h3>    " \
+           b"<h6 id='cobaia'></h6><br>    " \
+           b"<input type='number' id='peso' placeholder='Peso' style='text-align: center;width: 10vw;height: 2vh;'><br><br>    " \
+           b"<input type='number' id='altura' placeholder='Altura , em centimetros' style='text-align: center;width: 10vw;height: 2vh;'><br><br>    " \
+           b"<button type='submit' class='btn' onclick=(botao())>GO</button>     " \
+           b"</div>    " \
+           b"</body>    " \
+           b"<footer> <script type='text/javascript'>" \
            b"var peso = document.getElementById('peso');" \
            b"var altura = document.getElementById('altura');" \
            b"function botao(){" \
            b"var imc = peso.value/(altura.value*altura.value)*10000;" \
-           b"alert('IMC: '+imc);if(imc<18.50){document.getElementById('cobaia').innerHTML='<h6>Class: Magreza - Obesidade grau 0<h6>'}if(imc>=18.50 && imc<=24.9){document.getElementById('cobaia').innerHTML='<h6>Class: Normal - Obesidade grau 0<h6>'}if(imc>=25.00 && imc<=29.90){document.getElementById('cobaia').innerHTML='<h6>Class: Sobrepeso - Obesidade grau 1<h6> ' }if(imc>=30.00 && imc<=39.90){document.getElementById('cobaia').innerHTML='<h6>Class: Obesidade - Obesidade grau 2<h6>'}if(imc>=40.00){document.getElementById('cobaia').innerHTML='<h6>Class: Obesidade Grave - Obesidade grau 3<h6>'}}    </script>    </footer>    </html>    "
+           b"alert('IMC: '+imc);" \
+           b"if(imc<18.50){" \
+           b"document.getElementById('cobaia').innerHTML='<h6>Class: Magreza - Obesidade grau 0<h6>'}" \
+           b"if(imc>=18.50 && imc<=24.9){document.getElementById('cobaia').innerHTML='<h6>Class: Normal - Obesidade grau 0<h6>'}" \
+           b"if(imc>=25.00 && imc<=29.90){document.getElementById('cobaia').innerHTML='<h6>Class: Sobrepeso - Obesidade grau 1<h6> ' }" \
+           b"if(imc>=30.00 && imc<=39.90){document.getElementById('cobaia').innerHTML='<h6>Class: Obesidade - Obesidade grau 2<h6>'}" \
+           b"if(imc>=40.00){document.getElementById('cobaia').innerHTML='<h6>Class: Obesidade Grave - Obesidade grau 3<h6>'" \
+           b"}" \
+           b"}    " \
+           b"</script>    " \
+           b"</footer>    " \
+           b"</html>    "
 
     status = '200 OK'
     headers = [('Content-type', 'text/html')]
