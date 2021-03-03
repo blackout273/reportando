dd1e84cbadc9e1fe87d8764da59789d2e488bfca
blackout273 <69636059+blackout273@users.noreply.github.com>
2021-03-02 23:18:04 -0300

    atualização

--- contar.py
+++ contar.py
@@ -1,32 +1,82 @@
 def app(environ, start_response):
     """Simplest possible application object"""
 
-    data = b"<!DOCTYPE html>" \
-           b"<html=lang'pt-br'>" \
-           b"<head><title>Pipeline</title>" \
-           b"<style>*{" \
-           b"margin:0;padding:0;}" \
-           b".container{position:absolute;margin-left:50%;transform:translateX(-50%);margin-top:15%;font-size:2vw;text-align:center;}" \
+    data = b"<html lang='pt-br'>" \
+           b"<head>" \
+           b"<meta charset='utf-8'>" \
+           b"<title>Pipeline</title>" \
+           b"<style>" \
+           b"*{" \
+           b"margin:0;padding:0;" \
+           b"}" \
+           b".container{" \
+           b"position:absolute;margin-left:50%;" \
+           b"transform:translateX(-50%);margin-top:15%;" \
+           b"font-size:2vw;" \
+           b"text-align:center;" \
+           b"}" \
+           b".btn{" \
+           b"border: none;" \
+           b"background-color: white;" \
+           b"color: black;" \
+           b"width: 2vw;" \
+           b"height: 2vh;" \
+           b"border-radius: 10px;" \
+           b"transition: 0.5s ease;" \
+           b"}" \
+           b".btn:hover{" \
+           b"background-color: black;" \
+           b"color: white;" \
+           b"border: 2px solid white;" \
+           b"transition: 0.3s ease;" \
+           b"}" \
+           b"body{" \
+           b"transition: 0.5s ease;" \
+           b"font-family: 'Courier New', Courier, monospace;" \
+           b"}" \
+           b"body:hover{" \
+           b"background-color: black;" \
+           b"color: white;" \
+           b"transition: 0.3s ease;" \
+           b"}" \
            b"</style></head>" \
            b"<body>" \
-           b"<div class='container'><h1>Pipeline Completa</h1>" \
-           b"<h6>por favor insira o seu nome</h6>" \
-           b"<input type='text' id='nome' placeholder='Por favor, insira o seu nome'>" \
-           b"<button type='submit' onclick=(botao())>enviar</button>" \
-           b" </div>" \
+           b"<div class='container'><h3>Calcule seu Indice de Massa Corporal IMC</h3>" \
+           b"<h6 id='cobaia'></h6><br>" \
+           b"<input type='number' id='peso' placeholder='Peso' style='text-align: center;width: 10vw;height: 2vh;'><br><br>" \
+           b"<input type='number' id='altura' placeholder='Altura, em centimetros' style='text-align: center;width: 10vw;height: 2vh;'><br><br>" \
+           b"<button type='submit' class='btn' onclick=(botao())>GO</button>" \
+           b"</div>" \
            b"</body>" \
            b"<script type='text/javascript'>" \
-           b"var nome = document.getElementById('nome');" \
+           b"var peso = document.getElementById('peso');" \
+           b"var altura = document.getElementById('altura');" \
            b"function botao(){" \
-           b"alert('ola: '+nome.value);" \
-           b"}</script>" \
+           b"var imc = peso.value/(altura.value*altura.value)*10000" \
+           b"alert('IMC: '+imc);" \
+           b"if(imc<18.50){" \
+           b"document.getElementById('cobaia').innerHTML='<h6>Class: Magreza - Obesidade grau 0<h6>'" \
+           b"}if(imc>=18.50 && imc<=24.9){" \
+           b"document.getElementById('cobaia').innerHTML='<h6>Class: Normal - Obesidade grau 0<h6>'" \
+           b"}" \
+           b"if(imc>=25.00 && imc<=29.90){" \
+           b"document.getElementById('cobaia').innerHTML='<h6>Class: Sobrepeso - Obesidade grau 1<h6>' " \
+           b"}" \
+           b"if(imc>=30.00 && imc<=39.90){" \
+           b"document.getElementById('cobaia').innerHTML='<h6>Class: Obesidade - Obesidade grau 2<h6>'" \
+           b"}" \
+           b"if(imc>=40.00){" \
+           b"document.getElementById('cobaia').innerHTML='<h6>Class: Obesidade Grave - Obesidade grau 3<h6>'" \
+           b"}" \
+           b"}" \
+           b"</script>" \
            b"</html>"
-
 
     status = '200 OK'
     headers = [('Content-type', 'text/html')]
     start_response(status, headers)
     return [data]
 
-def somar(x,y):
-    return (x+y)
+
+def somar(x, y):
+    return (x + y)
