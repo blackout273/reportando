6598614749e4c1bcb42ff196c4bf9203c0910084
blackout273 <69636059+blackout273@users.noreply.github.com>
2021-03-02 21:45:53 -0300

    atualização

--- contar.py
+++ contar.py
@@ -1,30 +1,25 @@
+import socket
 def app(environ, start_response):
     """Simplest possible application object"""
-    data = b"<!DOCTYPE html>" \
-           b"<html=lang'pt-br'>" \
-           b"<head><title>Pipeline</title>" \
-           b"<style>*{" \
-           b"margin:0;padding:0;}" \
-           b".container{position:absolute;margin-left:50%;transform:translateX(-50%);margin-top:15%;font-size:2vw;text-align:center;}" \
-           b"</style></head>" \
-           b"<body>" \
-           b"<div class='container'><h1>Pipeline Completa</h1>" \
-           b"<h6>por favor insira o seu nome</h6>" \
-           b"<input type='text' id='nome' placeholder='Por favor, insira o seu nome'>" \
-           b"<button type='submit' onclick=(botao())>enviar</button>" \
-           b" </div>" \
-           b"</body>" \
-           b"<script type='text/javascript'>" \
-           b"var nome = document.getElementById('nome');" \
-           b"function botao(){" \
-           b"alert('ola: '+nome.value);" \
-           b"}</script>" \
-           b"</html>"
+    host=socket.gethostbyname(socket.gethostname())
+    port=6500
+    addr=(host,port)
+    data = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    data.bind((addr))
+    data.listen(1)
+    while True:
+        conn,host=data.accept()
+        print(" Conectado a : ", host)
+        status = '200 OK'
+        while True:
+            headers = [('Content-type', 'text/html')]
+            start_response(status, headers)
+            if not conn: break
+        print("conexao finalizada")
+        conn.close()
+        return [data]
 
-    status = '200 OK'
-    headers = [('Content-type', 'text/html')]
-    start_response(status, headers)
-    return [data]
 
 def somar(x,y):
     return (x+y)
+
